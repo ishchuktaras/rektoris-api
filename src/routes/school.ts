8782bd2beab3c5751dce04effaa5753d08c2a43d
Taras Ishchuk <ishchuktaras@gmail.com>
2025-02-05 16:41:08 +0100

    added school fetch

--- src/routes/school.ts
+++ src/routes/school.ts
@@ -6,7 +6,7 @@ const schoolRouter: Router = express.Router();
 
 schoolRouter.post("/schools", createSchool);
 schoolRouter.get("/schools", getSchools);
-schoolRouter.get("/schools:id", getSchoolById);
+schoolRouter.get("/schools/:id", getSchoolById);
 // schoolRouter.get("/api/v2/customers", getV2Customers);
 
 export default schoolRouter;
