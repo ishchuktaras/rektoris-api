16eee42d72ab349c66c79d5e2d21347c63e80048
Taras Ishchuk <ishchuktaras@gmail.com>
2025-02-06 19:58:02 +0100

    Modify parents

--- src/controllers/parents.ts
+++ src/controllers/parents.ts
@@ -1,33 +1,14 @@
+
 import { db } from "@/db/db";
 import { ParentCreateProps, TypedRequestBody } from "@/types/types";
 import { UserRole } from "@prisma/client";
 import { Request, Response } from "express";
 import { createUserService } from "./users";
 
-export const createParent = async (
-  req: TypedRequestBody<ParentCreateProps>,
-  res: Response
-): Promise<void> => {
+export const createParent = async (req: TypedRequestBody<ParentCreateProps>, res: Response): Promise<void> => {
   const data = req.body;
-  const {
-    nationalId,
-    phone,
-    email,
-    whatsappNumber,
-    title,
-    firstName,
-    lastName,
-    relationship,
-    address,
-    gender,
-    dateOfBirth,
-    nationality,
-    contactMethod,
-    occupation,
-    password,
-    user,
-  } = data;
-
+  const { nationalId, phone, email, whatsappNumber, title, firstName, lastName, relationship, address, gender, dateOfBirth, nationality, contactMethod, occupation, password, user, schoolId, schoolName} = data;
+  
   const formattedData = {
     nationalId,
     phone,
@@ -46,21 +27,20 @@ export const createParent = async (
     password,
     user: { connect: { id: user } },
     schoolName: data.schoolName,
-  };
+    schoolId: data.schoolId
+  };  
 
   try {
     // Check for existing entries
-    const [existingEmail, existingNationalId, existingPhoneNumber] =
-      await Promise.all([
-        db.parent.findUnique({ where: { email } }),
-        db.parent.findUnique({ where: { nationalId } }),
-        db.parent.findUnique({ where: { phone } }),
-      ]);
+    const [existingEmail, existingNationalId, existingPhoneNumber] = await Promise.all([
+      db.parent.findUnique({ where: { email } }),
+      db.parent.findUnique({ where: { nationalId } }),
+      db.parent.findUnique({ where: { phone } }),
+      
+    ]);
 
     if (existingNationalId) {
-      res
-        .status(409)
-        .json({ error: "Rodič s tímto občanským průkazem již existuje" });
+      res.status(409).json({ error: "Rodič s tímto občanským průkazem již existuje" });
       return;
     }
     if (existingEmail) {
@@ -72,27 +52,26 @@ export const createParent = async (
       return;
     }
 
-    // Create a parent as a user
-    const userData = {
-      email: data.email,
-      password: data.password,
-      role: "PARENT" as UserRole,
-      name: `${data.firstName} ${data.lastName}`,
-      phone: data.phone,
-      image: data.imageUrl,
-      schoolId: data.schoolId,
-      schoolName: data.schoolName,
-    };
-    const user = await createUserService(userData);
-    data.userId = user.id;
+     // Create a parent as a user
+        const userData = {
+          email: data.email,
+          password: data.password,
+          role: "PARENT" as UserRole,
+          name: `${data.firstName} ${data.lastName}`,
+          phone: data.phone,
+          image: data.imageUrl,
+          schoolId: data.schoolId,
+          schoolName: data.schoolName,
+        };
+        const user = await createUserService(userData);
+        data.userId = user.id;
+
 
     // Create a new parent
-    const newParent = await db.parent.create({
-      data: formattedData,
+    const newParent = await db.parent.create({ 
+      data: formattedData 
     });
-    console.log(
-      `Rodič byl úspěšně vytvořen: ${newParent.firstName} (${newParent.id})`
-    );
+    console.log(`Rodič byl úspěšně vytvořen: ${newParent.firstName} (${newParent.id})`);
     res.status(201).json({ data: newParent });
   } catch (error: any) {
     console.error("Database error:", error); // Add more logging here
@@ -100,10 +79,8 @@ export const createParent = async (
   }
 };
 
-export const getParents = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+export const getParents = async(req: Request, res: Response): Promise<void> =>
+{
   try {
     const parents = await db.parent.findMany({
       orderBy: {
@@ -111,12 +88,13 @@ export const getParents = async (
       },
     });
     res.status(200).json(parents);
-    return;
+    return
   } catch (error) {
     console.log(error);
     res.status(500).json({
       data: null,
-      error: "Něco se pokazilo",
+      error: "Něco se pokazilo"
     });
   }
-};
+}
+
